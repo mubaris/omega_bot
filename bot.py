e7500ead929953f3362e40255f69e7566fe53d19
aswanthkoleri <aswanth9495@gmail.com>
2018-01-27 05:28:47 +0530

    Add joke bot

--- bot.py
+++ bot.py
@@ -1,19 +1,21 @@
 import pprint
 import zulip
-import sys
-from crypto import Crypto
-from pnr import Pnr
-from mustread import Mustread
-
+import requests
+from geocode import Geocode
+from weather import Weather 
+from dict import Dictionary
+from joke import Joke
 p = pprint.PrettyPrinter()
-BOT_MAIL = "chunkzz-bot@chunkzz.zulipchat.com"
-
+BOT_MAIL = "awesome-bot@chunkzz.zulipchat.com"
+geo = Geocode()
+weather  = Weather()
+dict = Dictionary()
+joke = Joke()
 class ZulipBot(object):
 	def __init__(self):
 		self.client = zulip.Client(site="https://chunkzz.zulipchat.com/api/")
 		self.subscribe_all()
-		self.pnr = Pnr()
-		self.mustread = Mustread()
+		#self.crypto = Crypto()
 
 	def subscribe_all(self):
 		json = self.client.get_streams()["streams"]
@@ -24,38 +26,44 @@ class ZulipBot(object):
 		content = msg["content"].split()
 		sender_email = msg["sender_email"]
 		ttype = msg["type"]
-
+		name = msg["sender_full_name"]
+		p.pprint(msg)
 		print(content)
 
 		if sender_email == BOT_MAIL:
 			return 
 
+		print("yeah")
 
-		if content[0] == "omega" or content[0] == "@**omega**":
-			if content[1] == "pnr":
-				message = self.pnr.get_pnr(content[2])
-				self.client.send_message({
-					"type": "stream",
-					"subject": msg["subject"],
-					"to": msg["display_recipient"],
-					"content": message
-					})
-			elif content[1] == "read":
-				email = self.mustread.get_email(self.client.get_members(),msg["content"])
-				senderusername = self.mustread.get_username(self.client.get_members(),msg["sender_email"])
-				print(email)
-				self.client.send_message({
-				"type": "private",
-				"to": email,
-				"content": "**"+senderusername+"** mentioned you in must read ! \nThe message says : "+" ".join(content[2:])
-			})
-		elif "omega" in content:
+		if content[0] == "omega" and content[1] == "weather" :
+			# Write code here
+			place = content[2]
+			result = weather.getWeather(geo.convert(place))			
 			self.client.send_message({
 				"type": "stream",
 				"subject": msg["subject"],
 				"to": msg["display_recipient"],
-				"content": "Alas! Finally you called me :blush:"
+				"content": "**"+"Weather update of "+place+"**"+"\n"+"Summary : " + "**"+result["currently"]["summary"]+"**"+"\n"+"Temparature : " +"**"+ str(result["currently"]["temperature"])+"**" +'\n'
+				+"Apparent Temparature : "+"**"+str(result["currently"]["apparentTemperature"])+"**"+"\n"+"Dew Point : "+"**"+str(result["currently"]["dewPoint"])+"**"+"\n"+"Humidity : "+"**"+str(result["currently"]["humidity"])+"**"  
 				})
+		elif content[2] == "means" and content[0] == "omega" :
+			word = content[1]
+			result=dict.words(word)
+			print(result)
+			self.client.send_message({
+				"type": "stream",
+				"subject": msg["subject"],
+				"to": msg["display_recipient"],
+				"content": "**"+word+" means :"+"**"+'\n'+result  
+				})
+		elif content[1] == "tell" and content[2] == "a" and content[3] == "joke" and content[0] == "omega" :
+			text=joke.tellJoke()
+			self.client.send_message({
+				"type": "stream",
+				"subject": msg["subject"],
+				"to": msg["display_recipient"],
+				"content": text  
+				})		
 		else:
 			return
 
@@ -64,8 +72,4 @@ def main():
 	bot.client.call_on_each_message(bot.process)
 
 if __name__ == "__main__":
-	try:
-		main()
-	except KeyboardInterrupt:
-		print("Thanks for using Omega Bot. Bye!")
-		sys.exit(0)
+	main()
