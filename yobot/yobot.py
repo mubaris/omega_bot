42927df659ab516c1e7ea238ffe407969f421733
Anupam-dagar <dagaranupam@gmail.com>
2018-01-27 22:19:16 +0530

    add hackernews, motivate, gdrive in progress, url shortener

--- yobot/yobot.py
+++ yobot/yobot.py
@@ -3,13 +3,20 @@ import pprint
 import requests
 import os
 import json
-
+from motivate import Motivate
+from shorturl import Urlshortener
+from hackernews import Hackernews
 #initialise pprint
 p = pprint.PrettyPrinter()
 bot_id = 'yobot-bot@chunkzz.zulipchat.com'
+
 class Yobot(object):
 	def __init__(self):
 		self.client = zulip.Client(site="https://chunkzz.zulipchat.com/api")
+		self.motivate = Motivate()
+		self.shortenedurl = Urlshortener()
+		self.hacknews = Hackernews()
+		self.googledrive = Gdrive()
 		self.subscribe_streams()
 
 	def subscribe_streams(self):
@@ -18,7 +25,6 @@ class Yobot(object):
 			self.client.add_subscriptions([{'name': stream['name']}])
 
 	def process_bot(self, msg):
-		print("hithere")
 		all_streams = self.client.get_streams()['streams']
 		content = msg['content'].split()
 		sender_mail = msg['sender_email']
@@ -29,13 +35,14 @@ class Yobot(object):
 		if content[0] == 'yobot' or content[0] == '@**yobot**':
 			#motivationak quote
 			if content[1] == 'motivate':
-				data = requests.get('https://talaikis.com/api/quotes/random/').json()
+				quote_data = self.motivate.get_quote()
 				self.client.send_message({
-    				"type": "stream",
-    				"to": stream_name,
-    				"subject": stream_topic,
-    				"content": '"*' + data['quote'] + '*"\n -by ' + '**' + data['author'] + '**'
-				})
+					"type": "stream",
+					"to": stream_name,
+					"subject": stream_topic,
+					"content": quote_data
+					})
+
 			#send mail		
 			if content[1] == 'mail':
 				mail_data = MIMEText("hii bot")
@@ -44,33 +51,25 @@ class Yobot(object):
 				mail_data['subject'] = "try bot"
 				mail_data = (service.users().messages().send(userId=sender_email, body=mail_data).execute())
 				print (mail_data['id'])
+			
 			#url shortener
 			if content[1] == "shortenurl":
-				req_url = 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyDGsWyGP19sryyxXCmI3c9abhQYTIu_Iak'
-				payload = {'longUrl': content[2]}
-				headers = {'content-type': 'application/json'}
-				response = requests.post(req_url,data = json.dumps(payload), headers=headers).json()
+				short_url = self.shortenedurl.get_shorturl(content)
 				self.client.send_message({
 					"type": "stream",
 					"to": stream_name,
 					"subject": stream_topic,
-					"content": response['id']
+					"content": short_url
 					})
-			if content[1] == 'hackernews':
-				stories_id = requests.get('https://hacker-news.firebaseio.com/v0/topstories.json').json()
-				hack_data = ""
-				i = 1
-				for story_id in stories_id:
-					hacker_data = requests.get('https://hacker-news.firebaseio.com/v0/item/' + str(story_id) + '.json?print=pretty').json()
-					hack_data = hack_data + (str(i) + '. [' + hacker_data['title'] + '](' + hacker_data['url'] + ')\n')
-					i = i + 1
-					if (i == 11):
-						break
+
+			#hackernews
+			if content[1] == 'hackernews' or content[1] == 'hn' or content[1] == 'HN':
+				news = self.hacknews.get_hackernews()
 				self.client.send_message({
 						"type": "stream",
 						"to": stream_name,
 						"subject": stream_topic,
-						"content": hack_data
+						"content": news
 						})
 		else:
 			return
