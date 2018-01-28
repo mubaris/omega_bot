0117f90460c21b20066f4cba0d8f74209c29b326
aswinzz <iit2016106@iiita.ac.in>
2018-01-28 07:05:00 +0530

    add website

--- omega/motivate.py
+++
@@ -1,7 +0,0 @@
-import requests
-class Motivate(object):
-	def __init__(self):
-		self.data = requests.get('https://talaikis.com/api/quotes/random/').json()
-
-	def get_quote(self):
-		return "\"*" + self.data['quote'] + '*"\n -by ' + '**' + self.data['author'] + '**'
---
+++ website/README.md
@@ -0,0 +1,2 @@
+# omega.github.io
+This is the official website for omega bot.
